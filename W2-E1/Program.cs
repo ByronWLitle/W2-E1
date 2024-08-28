e19f2039680d7dfc483c4602583982b900973cda
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-28 15:26:12 -0500

    Added more comments

--- W2-E1/Program.cs
+++ W2-E1/Program.cs
@@ -10,7 +10,7 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            //Initialize global variable
+            //Initialize variables
             double grade = 0.0;
             int roundGrade = 0;
             //Prompts user to give grade percentage
@@ -20,25 +20,26 @@ namespace Exercise1
             //Rounds grade up if it is 0.05 to next grade
             roundGrade = Convert.ToInt32(Math.Round(grade));
             Console.WriteLine(roundGrade); //Output new grade
+            //Divide roundGrade by 10 to make it fall into case structure
             switch (roundGrade/10) 
             {
                 case 9:
-                    Console.WriteLine($"The letter grade for score {grade} is: A");
+                    Console.WriteLine($"The letter grade for score {grade} is: A"); //Output original grade and letter grade
                     break;
                 case 8:
-                    Console.WriteLine($"The letter grade for score {grade} is: B");
+                    Console.WriteLine($"The letter grade for score {grade} is: B"); //Output original grade and letter grade
                     break;
                 case 7:
-                    Console.WriteLine($"The letter grade for score {grade} is: C");
+                    Console.WriteLine($"The letter grade for score {grade} is: C"); //Output original grade and letter grade
                     break;
                 case 6:
-                    Console.WriteLine($"The letter grade for score {grade} is: D");
+                    Console.WriteLine($"The letter grade for score {grade} is: D"); //Output original grade and letter grade
                     break;
                 case 5:
-                    Console.WriteLine($"The letter grade for score {grade} is: F");
+                    Console.WriteLine($"The letter grade for score {grade} is: F"); //Output original grade and letter grade
                     break;
             }
-            
+            //Pause program to view
             Console.ReadLine();
         }
     }
